89adca17a2241ddf83439434c4ef06dc2af6b2ad
surelyourejoking <martin.robertandrew@gmail.com>
2017-04-12 21:35:42 +0800

    fixed bad import

--- 1_preprocessing/preprocessing.py
+++ 1_preprocessing/preprocessing.py
@@ -38,7 +38,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
                                                     random_state=0)
 
 # Feature scaling
-from sklearn.model_selection import StandardScaler
+from sklearn.preprocessing import StandardScaler
 
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
